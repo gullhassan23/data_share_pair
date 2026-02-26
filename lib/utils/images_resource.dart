df536769d2a8a4aa910120189ecc1095e2f4a521
Muhammad Usama <malikusama02486@gmail.com>
2026-02-26 10:23:30 +0500

    Extra images removed

--- lib/utils/images_resource.dart
+++ lib/utils/images_resource.dart
@@ -1,6 +1,5 @@
 class ImageRes {
   ImageRes._();
-  static const String logo = "assets/images/Logo.png";
   static const String sendFiles = "assets/icons/send.png";
   static const String recieveFiles = "assets/icons/upload.png";
 }
