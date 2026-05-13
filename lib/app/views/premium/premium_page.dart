7a92d33266b80f38482c4cb0ae97531fabd078d7
Muhammad Usama <198348821+usama-248@users.noreply.github.com>
2026-05-13 11:01:26 +0500

    Game analytics added event

--- lib/app/views/premium/premium_page.dart
+++ lib/app/views/premium/premium_page.dart
@@ -8,6 +8,7 @@ import 'package:share_app_latest/components/on_boardingbutton.dart';
 import 'package:share_app_latest/routes/app_navigator.dart';
 import 'package:share_app_latest/routes/app_routes.dart';
 import 'package:share_app_latest/services/analytics_screen_tracker.dart';
+import 'package:share_app_latest/services/game_analytics_service.dart';
 import 'package:share_app_latest/services/one_time_free_send_store.dart';
 import 'package:share_app_latest/services/subscription_iap_service.dart';
 
@@ -279,6 +280,9 @@ class PremiumPage extends GetView<PremiumController> {
                         child: IconButton(
                           onPressed: () {
                             AnalyticsScreenTracker.trackUiEvent(
+                              'Premium_Cross_button',
+                            );
+                            GameAnalyticsService.logDesignEvent(
                               'Premium_Cross_button',
                             );
                             AppNavigator.back();
